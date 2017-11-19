a33d3c0557b904763942eb48d471bede8d25198c
unknown <dadjin16@gmail.com>
2017-11-19 16:21:34 +0700

    How to use event

--- src/components/Pages/Course.js
+++ src/components/Pages/Course.js
@@ -1,6 +1,23 @@
 import React, {Component} from 'react'
 import Lesson from "./Lesson"
 class Course extends Component {
+   constructor(props) {
+       super(props)
+       this.handleClick4=this.handleClick4.bind(this);
+   }
+   
+    handleClick(){
+      alert("View1");
+    }
+    handleClick2(content){
+        alert(content);
+    }
+    handleClick3(){
+        alert(this.props.name);
+    }
+    handleClick4(){
+        alert(this.props.name);
+    }
     render() {
         return (
             <div className="col-xs-12 col-sm-4 col-md-4 col-lg-4">
@@ -13,6 +30,16 @@ class Course extends Component {
                        <p>{this.props.time}</p>
                        <p>{this.props.free}</p>
                        <Lesson/>
+                    </div>
+                    <div className="panel-footer">
+                       
+                       <div className="btn-group">
+                           <button onClick={this.handleClick} type="button"className="btn btn-default">View1</button>
+                           <button onClick={() => this.handleClick2("view2")} type="button" className="btn btn-success">View2</button>
+                           <button  onClick={this.handleClick3.bind(this)} type="button" className="btn btn-danger">View3</button>
+                           <button  onClick={this.handleClick4} type="button" className="btn btn-red">View4</button>
+                       </div>
+                       
                     </div>
                 </div>
             </div>
